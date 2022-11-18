af61cf8dd3d73643665b836e403837c7c2af8d41
your_username <bk185165@ncr.com>
2022-11-18 13:45:29 +0530

    Adding Bootstrap in this project for css and Router

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
-import  './App.css';
-import Car from './car';
-import sunitaimginApp from './su.jpg';
-import su2 from './su2.jpg';
-import su3 from './su3.jpg';
-import  GitProfs from './githubDatabase';
-import SuCard  from './Card';
+import "./App.css";
+import Car from "./car";
+import sunitaimginApp from "./su.jpg";
+import su2 from "./su2.jpg";
+import su3 from "./su3.jpg";
+import GitProfs from "./githubDatabase";
+import SuCard from "./Card";
+import Layout from "./Layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // aading my me
 //Add pull request to make changes authorising by myself
-const ArrowFunc=(props)=>{
-
-
-
-
-             return (
-               <div>
-
-               
-                   <div>Returning the argument + {props.message}</div>
-                   <div>{props.value}</div>
-
-               </div>    
-             ) ;  
-
-
-
-            }
-
-
-
-
+const ArrowFunc = (props) => {
+  return (
+    <div>
+      <div>Returning the argument + {props.message}</div>
+      <div>{props.value}</div>
+    </div>
+  );
+};
 
 function App() {
-
-  let a=10;
-  const s=90;
-
+  let a = 10;
+  const s = 90;
 
   return (
     <div>
+      <BrowserRouter>
+        
+        <Routes>
 
-                 {/* <ArrowFunc message="00000000000000000000000" value="230"/> */}
-            
-               
+          <Route>
+          <Route path="/" element={<Layout />}>
+          <Route index element={ <GitProfs />} />
+          <Route path="Brijesh" element={<ArrowFunc message="00000000000000000000000" value="230"/>} />
+          <Route path="Sunita" element={<SuCard />} />
+          {/* <Route path="*" element={<NoPage />} /> */}
+        </Route>            
+          </Route>
+        </Routes>
 
-                 {/* <img src="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg" className="SunitaCss"/>  */}
-             
-                 {/* <Car name="Sunil" description="Sunita is also pagal" sunita={su3}/> */}
-                 <GitProfs/>
+      </BrowserRouter>
+      {/* <ArrowFunc message="00000000000000000000000" value="230"/> */}
 
+      {/* <img src="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg" className="SunitaCss"/>  */}
+
+      {/* <Car name="Sunil" description="Sunita is also pagal" sunita={su3}/> */}
+      {/* <GitProfs /> */}
     </div>
-  )
-
+  );
 }
 
 export default App;
---
+++ src/Layout.js
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <ul className="container-fluid">
+          <li className="navbar-brand">
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Brijesh">Brijesh</Link>
+          </li>
+          <li>
+            <Link to="/Sunita">Sunita</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
+
+export default Layout;
