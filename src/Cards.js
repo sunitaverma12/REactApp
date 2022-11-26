ee7c48e4d78f335d5c1151fed6b266f1efb493b4
brijeshchauhan12 <brijeshkme@gmail.com>
2022-11-26 12:48:39 +0530

    Nov26 12:48 brijesh

--- src/Cards.js
+++ src/Cards.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button, Container } from 'reactstrap';
-  import su5 from "./su5.jpg";
-  import "./Cards_style.css";
-  
- 
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Container,
+} from "reactstrap";
+import su5 from "./su5.jpg";
+import "./Cards_style.css";
 
 const Cards = (props) => {
   return (
-    
-        <Container >
-      <Card  className="box" style={{width:"20%"}} >
-        <CardImg   src={su5} alt="Card image cap" />
+  
+      <Card className="box" style={{ width: "20",height:"20%" }}>
+        <CardImg src={su5} />
         <CardBody>
           <CardTitle>Card title</CardTitle>
-          
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <button   style={{color:"red"}}>Button</button>
+
+          <CardText>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </CardText>
+          <button style={{ color: "red" }}>Button</button>
         </CardBody>
       </Card>
-      </Container>
     
   );
 };
-<hr/>
 
 export default Cards;
